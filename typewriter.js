0f3b893ba82841d0711d6cbb89933a6db78792a9
rahomohamed <rahojmohamed@gmail.com>
2020-02-11 17:23:16 +0000

    new files

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+setTimeout(() => {
+  process.stdout.write(sentence[i]);
+}, 50 * i); // <= 1s delay to make it noticeable. Can dial it down later.
+}
